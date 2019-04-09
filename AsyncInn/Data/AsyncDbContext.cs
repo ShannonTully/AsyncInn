f3150f59a5da1518d1fa27a16d63ab25edcb859d
ShannonTully <scatman.st@gmail.com>
2019-04-08 22:04:01 -0700

    Woot

--- AsyncInn/Data/AsyncDbContext.cs
+++ AsyncInn/Data/AsyncDbContext.cs
@@ -18,6 +18,121 @@ namespace AsyncInn.Data
         {
             modelBuilder.Entity<RoomAmenities>().HasKey(ra => new { ra.AmenitiesID, ra.RoomID });
             modelBuilder.Entity<HotelRoom>().HasKey(hr => new { hr.HotelID, hr.RoomNumber });
+
+            modelBuilder.Entity<Hotel>().HasData(
+               new Hotel
+               {
+                   ID = 1,
+                   Name = "place1",
+                   StreetAddress = "1 0th st",
+                   City = "city1",
+                   State = "state1",
+                   Phone = "111",
+               },
+               new Hotel
+               {
+                   ID = 2,
+                   Name = "place2",
+                   StreetAddress = "2 0th st",
+                   City = "city2",
+                   State = "state2",
+                   Phone = "222",
+               },
+               new Hotel
+               {
+                   ID = 3,
+                   Name = "place3",
+                   StreetAddress = "3 0th st",
+                   City = "city3",
+                   State = "state3",
+                   Phone = "333",
+               },
+               new Hotel
+               {
+                   ID = 4,
+                   Name = "place4",
+                   StreetAddress = "4 0th st",
+                   City = "city4",
+                   State = "state4",
+                   Phone = "444",
+               },
+               new Hotel
+               {
+                   ID = 5,
+                   Name = "place5",
+                   StreetAddress = "5 0th st",
+                   City = "city5",
+                   State = "state5",
+                   Phone = "555",
+               }
+               );
+
+            modelBuilder.Entity<Room>().HasData(
+                new Room
+                {
+                    ID = 1,
+                    Name = "room1",
+                    Layout = 1,
+                },
+                new Room
+                {
+                    ID = 2,
+                    Name = "room2",
+                    Layout = 2,
+                },
+                new Room
+                {
+                    ID = 6,
+                    Name = "room6",
+                    Layout = 6,
+                },
+                new Room
+                {
+                    ID = 3,
+                    Name = "room3",
+                    Layout = 3,
+                },
+                new Room
+                {
+                    ID = 4,
+                    Name = "room4",
+                    Layout = 4,
+                },
+                new Room
+                {
+                    ID = 5,
+                    Name = "room5",
+                    Layout = 5,
+                }
+               );
+
+            modelBuilder.Entity<Amenities>().HasData(
+                new Amenities
+                {
+                    ID = 1,
+                    Name = "Amenities1",
+                },
+                new Amenities
+                {
+                    ID = 2,
+                    Name = "Amenities2",
+                },
+                new Amenities
+                {
+                    ID = 3,
+                    Name = "Amenities3",
+                },
+                new Amenities
+                {
+                    ID = 4,
+                    Name = "Amenities4",
+                },
+                new Amenities
+                {
+                    ID = 5,
+                    Name = "Amenities5",
+                }
+               );
         }
 
         public DbSet<Room> Room { get; set; }
---
+++ AsyncInn/Migrations/20190409025146_newthing.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AsyncInn.Migrations
+{
+    public partial class newthing : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Amenities",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Amenities1" },
+                    { 2, "Amenities2" },
+                    { 3, "Amenities3" },
+                    { 4, "Amenities4" },
+                    { 5, "Amenities5" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Hotel",
+                columns: new[] { "ID", "City", "Name", "Phone", "State", "StreetAddress" },
+                values: new object[,]
+                {
+                    { 1, "city1", "place1", "111", "state1", "1 0th st" },
+                    { 2, "city2", "place2", "222", "state2", "2 0th st" },
+                    { 3, "city3", "place3", "333", "state3", "3 0th st" },
+                    { 4, "city4", "place4", "444", "state4", "4 0th st" },
+                    { 5, "city5", "place5", "555", "state5", "5 0th st" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Room",
+                columns: new[] { "ID", "Layout", "Name" },
+                values: new object[,]
+                {
+                    { 1, 1, "room1" },
+                    { 2, 2, "room2" },
+                    { 6, 6, "room6" },
+                    { 3, 3, "room3" },
+                    { 4, 4, "room4" },
+                    { 5, 5, "room5" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Amenities",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Hotel",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Hotel",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Hotel",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Hotel",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Hotel",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Room",
+                keyColumn: "ID",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Room",
+                keyColumn: "ID",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Room",
+                keyColumn: "ID",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Room",
+                keyColumn: "ID",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Room",
+                keyColumn: "ID",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Room",
+                keyColumn: "ID",
+                keyValue: 6);
+        }
+    }
+}
